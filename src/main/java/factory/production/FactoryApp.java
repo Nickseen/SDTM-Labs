94bd0a4cbfb91739d07fc7446075d4950ab11de9
Nicolashaaa <npetcov@mail.ru>
2025-10-22 13:59:51 +0300

    Visual Beauty

--- src/main/java/factory/production/FactoryApp.java
+++ src/main/java/factory/production/FactoryApp.java
@@ -1,15 +1,41 @@
 package factory.production;
 
 import factory.car.*;
+import java.util.Scanner;
 
 public class FactoryApp {
     public void run() {
         CarFactory factory = new CarFactory();
+        Scanner scanner = new Scanner(System.in);
 
-        Car gasCar = factory.createCar(CarType.GAS);
-        gasCar.produce();
+        System.out.println("=== Car Factory - SOLID Principles Demo ===\n");
 
-        Car electricCar = factory.createCar(CarType.ELECTRIC);
-        electricCar.produce();
+        while (true) {
+            System.out.println("1. Produce Gas Car");
+            System.out.println("2. Produce Electric Car");
+            System.out.println("3. Exit");
+            System.out.print("Choose option: ");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    Car gasCar = factory.createCar(CarType.GAS);
+                    gasCar.produce();
+                    System.out.println();
+                    break;
+                case 2:
+                    Car electricCar = factory.createCar(CarType.ELECTRIC);
+                    electricCar.produce();
+                    System.out.println();
+                    break;
+                case 3:
+                    System.out.println("Goodbye!");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Invalid option!\n");
+            }
+        }
     }
 }
